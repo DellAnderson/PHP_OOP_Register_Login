33ac93b86c65a8c630afe050f52c0b63af25f262
lka <lka>
2017-01-16 18:57:54 -0800

    DB class working (with excessive debug echoes)

--- index.php
+++ index.php
@@ -6,17 +6,21 @@ require_once 'core/init.php';
 //$db = new DB(); //won't work for singleton pattern
 
 //DB::getInstanceDB(); //create a single database connection
-$user = DB::getInstanceDB()->makeQuery("SELECT username FROM users WHERE username = ? or username = ?", array(
-  "billy",
-  "bob")
-);
+// $user = DB::getInstanceDB()->makeQuery("SELECT username FROM users WHERE username = ? or username = ?", array(
+//   "billy",
+//   "bob")
+// );
 
-$user = DB::getInstanceDB()->makeQuery("SELECT username FROM users");
+$user = DB::getInstanceDB()->get('users', array('username', '=','alex'));
+//$user = DB::getInstanceDB()->get('users', array('username', '=','billy'));
+
+//$user = DB::getInstanceDB()->makeQuery("SELECT username FROM users");
 
 //echo "Were here!<br>"; //debug
-
-// if($user->error() == true){
-//   echo 'Query generated an error. Please check query syntax & spelling.<br>';
-// } else{
-//   echo 'ΟΚ!<br>';
-// }
+//var_dump($user);
+//if($user->getError()){
+if(!$user->getCount()){
+  echo 'Error. Please check query syntax, spelling, and whether table data exists.<br>';
+} else{
+  echo 'ΟΚ!<br>';  //user must exist to pass this
+}
